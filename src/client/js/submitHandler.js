ed288c0dfff24795a4aaff94037f1ca3a098cbab
NaifS01 <86309709+NaifS01@users.noreply.github.com>
2023-01-05 14:17:36 +0300

    extra more comments

--- src/client/js/submitHandler.js
+++ src/client/js/submitHandler.js
@@ -1,10 +1,16 @@
 import { UpdateUI } from "./udUi";
 
 function formHnadler() {
+    // For all the data coming from API
     let projectData = {};
     console.log("im in the function")
+    //getting the name of the city by id 
     const cityName = document.getElementById("input").value
+    //getting the date entered by the user 
     const date = document.getElementById("input2").value
+    
+    //transfer date to Date should be yy-mm-dd
+    //the getting the dates and calculate the differences between the NOW date and the date INPUTED by the user
     let countDown = Date.parse(date);
     let now = new Date().getTime();
     let distance = countDown - now;
@@ -21,6 +27,7 @@ function formHnadler() {
 
 
 
+//Post function 
 const postData = async (url = '', data = {}) => {
     const res = await fetch(url, {
         method: "POST",
